eb623af1af1f587cc73138d4acb8d8c2cb6e03a1
John Bender <john.bender@gov.ab.ca>
2020-03-23 16:39:06 -0600

    added test

--- atms/specific.js
+++ atms/specific.js
@@ -140,12 +140,14 @@ function ATMS(params) {
     switch (window.location.pathname) {
         case '/ram/Default.aspx':
             // viewing all of our offerings
+            console.log('viewing Sales Homepage');
             gtag('event', 'page_view', {
                 'name' : 'Sales Homepage'
             });
             break;
         case '/ram/DateSelection.aspx':
             // user is checking calendar for dates for general admission
+            console.log('viewing Admission date selection');
             gtag('event', 'page_view', {
                 'name' : 'Admission date selection'
             });
@@ -153,94 +155,9 @@ function ATMS(params) {
         case '/ram/Selection.aspx':
             // user has selected a date or chosen a mammoth pass
             if (params.sch) {
-                // presence of variable "sch" indicates a date is chosen for general admission
-                // grab the date (not sure if I can find a place for it, however)
-                var selectedDate = $('.ItemDetails h4').text();
-                // set an onSubmit event with the "add to order" button to grab the selection(s)
-                $('#AddToOrder').on('click', function(e) {
-                    var adult = $('.ErrorInput').eq(0).val();
-                    var senior = $('.ErrorInput').eq(1).val();
-                    var youth = $('.ErrorInput').eq(2).val();
-                    var child = $('.ErrorInput').eq(3).val();
-
-                    if (adult > 0) {
-                        ga('ec:addProduct', {
-                        'id': '1',
-                        'name': 'Adult Admission',
-                        'category': 'Admission',
-                        'price': '19.00',
-                        'quantity': adult
-                        });
-                    }
-                    if (senior > 0) {
-                        ga('ec:addProduct', {
-                        'id': '2',
-                        'name': 'Senior Admission',
-                        'category': 'Admission',
-                        'price': '14.00',
-                        'quantity': senior
-                        });
-                    }
-                    if (youth > 0) {
-                        ga('ec:addProduct', {
-                        'id': '3',
-                        'name': 'Youth Admission',
-                        'category': 'Admission',
-                        'price': '10.00',
-                        'quantity': youth
-                        });
-                    }
-                    if (child > 0) {
-                        ga('ec:addProduct', {
-                        'id': '4',
-                        'name': 'Child Admission',
-                        'category': 'Admission',
-                        'price': '0.00',
-                        'quantity': child
-                        });
-                    }
-                    ga('send', 'pageview');
-
-                    return true;
-                });
+                
             } else {
-                $('#AddToOrder').on('click', function(e) {
-                    var mammothPassType = "";
-                    var mammothPassPrice = "";
-                    var quantity = $('.ErrorInput').val();
 
-                    switch (params.item) {
-                        case 16:
-                            mammothPassPrice = "35.00";
-                            mammothPassType = "Adult Mammoth Pass";
-                            break;
-                        case 17:
-                            mammothPassPrice = "30.00";
-                            mammothPassType = "Senior Mammoth Pass";
-                            break;
-                        case 18:
-                            mammothPassPrice = "20.00";
-                            mammothPassType = "Youth Mammoth Pass";
-                            break;
-                        case 19:
-                            mammothPassPrice = "0.00";
-                            mammothPassType = "Child Mammoth Pass";
-                            break;
-                        case 20:
-                            mammothPassPrice = "70.00";
-                            mammothPassType = "Family Mammoth Pass";
-                            break;
-                    }
-
-                    ga('ec:addProduct', {
-                    'id': params.item,
-                    'name': mammothPassType,
-                    'category': 'Mammoth Pass',
-                    'price': mammothPassPrice,
-                        'quantity': quantity
-                    });
-                    ga('send', 'pageview');
-                });
             }
 
             break;
