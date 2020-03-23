e4bdb66d9e95cb1d4db9c63411bb8e5ce366b428
John Bender <john.bender@gov.ab.ca>
2020-03-23 14:28:58 -0600

    updated script

--- beanstream/specific.js
+++ beanstream/specific.js
@@ -4,7 +4,7 @@
 */
 function BeanStream() {
     // event registrations
-    $('input[name="trnCardType"]').on('change', function() {
+    $('select[name="trnCardType"]').on('change', function() {
         console.log('changed card to ' + this.val());
     });
 
