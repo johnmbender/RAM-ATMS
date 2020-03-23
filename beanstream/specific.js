d34678437089022998c9e4e6188658cb5befb7bd
John Bender <john.bender@gov.ab.ca>
2020-03-23 14:26:18 -0600

    testing event

--- beanstream/specific.js
+++ beanstream/specific.js
@@ -3,12 +3,16 @@
  # @beanstream/specific.js
 */
 function BeanStream() {
+    // event registrations
+    $('input[name="trnCardType"]').on('change', function() {
+        console.log('changed card to ' + this.val());
+    });
+
     // hard data lives here
     switch (window.location.pathname) {
         case '/scripts/payment/payment.asp':
             // ref1 is our key for confirmation pointing to ATMS' "order" variable
             // trnAmount is the total dollar value processing
-            print('let us see...');
             gtag('event', 'page_view', {
                 'name' : 'Credit Card form'
             });
