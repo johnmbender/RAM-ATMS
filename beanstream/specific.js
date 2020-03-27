4db405552aea294b9b9173eadbf1ed03cff61701
John Bender <john.bender@gov.ab.ca>
2020-03-27 16:54:35 -0600

    Stable

--- beanstream/specific.js
+++ beanstream/specific.js
@@ -2,51 +2,33 @@
  * 
  # @beanstream/specific.js
 */
-var params;
-
-(() => {
-    console.log('start');
-    params = retrieveURLparams();
-    // event-catching
-    eventCatchers();
-    pageLoad();
-    console.log('end');
-})();
+var params = retrieveURLparams();
+eventCatchers();
+pageLoad();
 
 function eventCatchers() {
     // event catchers here, if necessary
-    console.log('eventCatchers');
 }
 
 function pageLoad() {
-    console.log('pageLoad');
     // hard data lives here
-    console.log(window.location.pathname);
     switch (window.location.pathname) {
         case '/scripts/payment/payment.asp':
-            console.log('seriously WTF');
             // ref1 is our key for confirmation pointing to ATMS' "order" variable
             // trnAmount is the total dollar value processing
             var cartValue = 0;
             if (params == null) {
-                console.log('no params?:');
-                console.log(params);
                 return true;
             }
             if (params.trnAmount == null) {
-                console.log('no trnAmount?:');
-                console.log(params.trnAmount);
                 return true;
             }
 
             cartValue = params.trnAmount;
-            console.log('cart value: ' + cartValue);
             gtag('event', 'checkout_progress', {
                 'value' : cartValue,
                 'checkout_step' : 2
             });
-            console.log("I gtag'd?");
             break;
     }
-    console.log('end of pageLoad()');
 }
