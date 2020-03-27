16df26ff4fd96991dd14c9455a4eb43312a77af9
John Bender <john.bender@gov.ab.ca>
2020-03-27 10:53:05 -0600

    ...wtf

--- beanstream/specific.js
+++ beanstream/specific.js
@@ -21,28 +21,27 @@ function eventCatchers() {
 function pageLoad() {
     console.log('pageLoad');
     // hard data lives here
-    switch (window.location.pathname) {
-        case '/scripts/payment/payment.asp':
-            // ref1 is our key for confirmation pointing to ATMS' "order" variable
-            // trnAmount is the total dollar value processing
-            var cartValue = 0;
-            if (params.length == 0) {
-                return true;
-            }
-            if (params.trnAmount.length == 0) {
-                return true;
-            }
+    // switch (window.location.pathname) {
+    //     case '/scripts/payment/payment.asp':
+    //         // ref1 is our key for confirmation pointing to ATMS' "order" variable
+    //         // trnAmount is the total dollar value processing
+    //         var cartValue = 0;
+    //         if (params.length == 0) {
+    //             return true;
+    //         }
+    //         if (params.trnAmount.length == 0) {
+    //             return true;
+    //         }
 
-            cartValue = params.trnAmount;
-            gtag('event', 'checkout_progress', {
-                'value' : cartValue,
-                'checkout_step' : 2
-            });
-            break;
-        default:
-            // CC declined, etc. capture? or no?
-            break;
-        
-    }
+    //         cartValue = params.trnAmount;
+    //         gtag('event', 'checkout_progress', {
+    //             'value' : cartValue,
+    //             'checkout_step' : 2
+    //         });
+    //         break;
+    //     default:
+    //         // CC declined, etc. capture? or no?
+    //         break;
+    // }
     console.log('end of pageLoad()');
 }
